f4c1298ffaf64be5c925eb63761d76a173333de5
nabratte <61842410+nabratte@users.noreply.github.com>
2022-02-06 21:10:49 -0300

    get temperaments

--- PI-Dogs-main/api/src/routes/index.js
+++ PI-Dogs-main/api/src/routes/index.js
@@ -42,6 +42,33 @@ const getAllDogs = async ()=>{
     return generalInfo; 
 }
 
+const getAllTemps = async()=>{
+    const  apiInfo = await getApiInfo();
+    
+    let apiTemps = "";
+    apiInfo.forEach(d =>{
+        if(!d.temperament){
+            d.temperament="";
+        }
+        apiTemps = d.temperament+ ", "+apiTemps;
+    });
+    const temperamentsArr = new Set(apiTemps.split(", ").sort());
+    const result = [...temperamentsArr];
+    result.shift();
+    return result;
+}
+const setDbTemperaments = async ()=>{
+    const allTemperaments = await getAllTemps();
+    allTemperaments.forEach((t) => {
+        Temperament.findOrCreate({
+          where: { name: t },
+        });
+    })
+}
+
+const getDbTemperaments = async () => {
+    return await Temperament.findAll();
+  };
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
@@ -72,16 +99,19 @@ router.get('/dogs', async(req,res)=>{
 // Obtener un listado de las razas de perro
 // Debe devolver solo los datos necesarios para la ruta principal
 router.get('/dogs', async(req,res)=>{
+    try{
     let dogList = await getAllDogs();
-    res.status(200).send(dogList.map((dog) => {
+    var perros = dogList.map((dog) => {
         return {
           name: dog.name,
           weight: dog.weight,
           image: dog.image,
           temperament: dog.temperament,
         };
-      })
-    );
+    })
+    res.status(200).send(perros)
+    }
+    catch(error){return error}
 })
 
 
@@ -100,12 +130,14 @@ router.get('/dogs/:id',async(req,res)=>{
 // [ ] GET /temperament:
 // Obtener todos los temperamentos posibles
 // En una primera instancia deberán obtenerlos desde la API externa y guardarlos en su propia base de datos y luego ya utilizarlos desde allí
-router.get('/temperament',async(req,res)=>{
-    let temperamentList = await Temperament.findAll();
-    res.status(200).send(temperamentList)
-})
+router.get("/temperament", async (req, res) => {
+  await setDbTemperaments();
+  const allTemperaments = await getDbTemperaments();
+  res.status(200).json(allTemperaments);
+});
 // [ ] POST /dog:
 // Recibe los datos recolectados desde el formulario controlado de la ruta de creación de raza de perro por body
 // Crea una raza de perro en la base de datos
+
 
 module.exports = router;
